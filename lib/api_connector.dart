dd9e83449b7997fbcea2d0c7ab691f8112eee914
Ju1yan <110489258+Ju1yan@users.noreply.github.com>
2026-05-18 11:03:50 +0200

    8- Commit2 added error handling capabilities
    added error handling for case 4, 5 and statuscode 429

--- lib/api_connector.dart
+++ lib/api_connector.dart
@@ -1,14 +1,18 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'dart:async';
 
 /*USAGE: List<Question> varName = await fetchQuestions(amount of questions);
-varName[var].type for the type of question
-varName[var].question for the question string
-varName[var].correctAnswer for the correct Answer
-varName[var].incorrectAnswers for the incorrect Answer
+varName[questionNum].type for the type of question
+varName[questionNum].question for the question string
+varName[questionNum].correctAnswer for the correct Answer
+varName[questionNum].incorrectAnswers for the incorrect Answer
+
+for later use .difficulty, .category are also available
 */
 
 String? token = "3b3"; //false token, let the code create a new one through the exception
+int excCntr = 0;
 
 
 class QuestionData{  //because the api is staged we have to first handle responsecodes
@@ -76,6 +80,9 @@ Future<List<Question>> fetchQuestions(int amountQuestions) async { // fetches an
           jsonDecode(response.body) as Map<String, dynamic>);
       switch (data.responseCode) {
         case 0: //OK return questions
+          if(excCntr != 0) {
+            excCntr = 0; //resetting exception counter;
+          }
           return data.questions;
         case 1: //no questions left
           throw Exception("No Results, not enough questions left");
@@ -87,18 +94,33 @@ Future<List<Question>> fetchQuestions(int amountQuestions) async { // fetches an
           token = newToken.token;
           return fetchQuestions(amountQuestions);
         case 4: // no questions available who havent been used - reset token
-          throw Exception(
-              "Token empty, token has exhausted all possible questions");
-      //TODO:add logic to reset token
+          //getting new token and replacing it
+        //TODO: Error message in user interface telling user token has been reset
+          Token newToken = await createToken();
+          token = newToken.token;
+        return fetchQuestions(amountQuestions);
+
         case 5: //too fast request, probably not used anymore because the statuscode handles that with 429
-          throw Exception("Rate limit: retry in 5 seconds");
-      //TODO: add timer to retry in 5 seconds
+        if(excCntr < 3) {
+          excCntr++; //exception counter
+          await Future.delayed(Duration(seconds: 5));
+          return fetchQuestions(amountQuestions);
+        } else {
+          excCntr = 0;
+          throw Exception("Unknown error, failed 3 times fetching questions");
+        }
         default: //unknown code
           throw Exception("Unknown Error: code ${data.responseCode}");
       }
     } else if (response.statusCode == 429) { //handles too fast requests
-      throw Exception('too many requests in 5S');
-      //TODO: add timer and retry in 5 sec
+      if(excCntr < 3) {
+        excCntr++; //exception counter
+        await Future.delayed(Duration(seconds: 5));
+        return fetchQuestions(amountQuestions);
+      } else {
+        excCntr = 0;
+        throw Exception("Unknown error, failed 3 times fetching questions");
+      }
     } else {
       throw Exception('http request failed'); //unknown error
     }
