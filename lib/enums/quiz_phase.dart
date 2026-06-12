3fb328dd56d304175da45d66080130c9a879b382
Severin <severinkoeberl256@gmail.com>
2026-06-12 13:36:32 +0200

    correct answer and player answers visually implemented

---
+++ lib/enums/quiz_phase.dart
@@ -0,0 +1,4 @@
+enum QuizPhase {
+  answering,
+  showingResults,
+}
--- lib/gameState/base_game_state.dart
+++ lib/gameState/base_game_state.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:lan_quiz/enums/Mode.dart';
+import 'package:lan_quiz/enums/quiz_phase.dart';
 import 'package:lan_quiz/enums/joker_type.dart';
 import 'package:lan_quiz/enums/uiState.dart';
-import 'package:lan_quiz/screens/leaderboard_screen.dart';
-import 'package:lan_quiz/main.dart';
-import '../client_question.dart';
 
 abstract class BaseGameState extends ChangeNotifier {
   // Shared UI state variables
@@ -20,6 +18,9 @@ abstract class BaseGameState extends ChangeNotifier {
   Mode mode = Mode.none;
   String? localIp;
   UiState uiState = UiState.home;
+  QuizPhase quizPhase = QuizPhase.answering;
+  int correctAnswerIndex = -1;
+  Map<String,String> playerAnswersThisRound = {}; // saves player name -> Answer
 
   // Shared interface that both components implement
   void processNetworkMessage(String msg);
