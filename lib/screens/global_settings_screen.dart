b261620bfe94fcbe51f07da45f0830fc11e2fcc5
Julian Mueller <muellerjulian12@gmail.com>
2026-06-16 14:57:09 +0200

    added category to question screen, fixed bug

--- lib/screens/global_settings_screen.dart
+++ lib/screens/global_settings_screen.dart
@@ -3,7 +3,6 @@ import "dart:async";
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import '../gameState/base_game_state.dart';
 import '../gameState/host_game_state.dart';
 
 
