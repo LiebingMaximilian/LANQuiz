72bab25a84a721a1bbfc325b48eabf867bcab08d
Julian Mueller <muellerjulian12@gmail.com>
2026-06-18 16:58:52 +0200

    configured sound to not interrupt on ios and android

--- lib/sound_manager.dart
+++ lib/sound_manager.dart
@@ -1,8 +1,23 @@
 import 'package:audioplayers/audioplayers.dart';
 
+ void audioInit() async{
+   await AudioPlayer.global.setAudioContext(AudioContext(
+      iOS: AudioContextIOS(
+        category: AVAudioSessionCategory.ambient, //so it does't interrupt music and respects silence mode
+    ),
+    android: const AudioContextAndroid(
+      contentType: AndroidContentType.sonification, //sonification is for user action therefore doesnt interrupt music
+      usageType: AndroidUsageType.assistanceSonification,
+      audioFocus: AndroidAudioFocus.none,  //important says android it doesnt want audio focus at all
+
+    ),
+    ));
+    print("audioconfig initialized");
+  }
+
 class SoundManager {
   static final AudioPlayer _player = AudioPlayer();
-
+  
   static Future<void> answerSelected() async {
     await _player.play(AssetSource('sounds/answerSelected.mp3'));
   }
